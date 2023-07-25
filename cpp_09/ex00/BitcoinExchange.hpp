75b9ba93cd350c0ed65d4f2de4d305781bc7c0f6
WallBlade <zakary.elkassimi75015@gmail.com>
2023-07-25 20:33:29 +0200

    CPP_09 : wip

--- cpp_09/ex00/BitcoinExchange.hpp
+++ cpp_09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,15 @@
 /*   By: zel-kass <zel-kass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 15:17:25 by zel-kass          #+#    #+#             */
-/*   Updated: 2023/07/23 15:17:26 by zel-kass         ###   ########.fr       */
+/*   Updated: 2023/07/25 17:39:25 by zel-kass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef __BITCOINEXCHANGE_HPP__
+#define __BITCOINEXCHANGE_HPP__
+
+#include <iostream>
+#include <fstream>
+
+
+#endif
--- cpp_09/ex00/main.cpp
+++ cpp_09/ex00/main.cpp
@@ -6,7 +6,18 @@
 /*   By: zel-kass <zel-kass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/23 15:17:27 by zel-kass          #+#    #+#             */
-/*   Updated: 2023/07/23 15:17:28 by zel-kass         ###   ########.fr       */
+/*   Updated: 2023/07/25 17:41:37 by zel-kass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+
+int main(int ac, char **av) {
+	std::ifstream in(av[1]);
+	if (ac != 2 || !in.is_open()) {
+		std::cout << "Error: could not open file." << std::endl;
+		return (1);
+	}
+
+	return (0);
+}
